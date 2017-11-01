cb8e1b34c7fb940447ed5d60e122137526a8511f
Alemalakra <alemalakra@gmail.com>
2017-11-01 05:49:25 -0600

    Updated for 2.0

--- test.php
+++ test.php
@@ -4,6 +4,8 @@ require('xwaf.php'); // BEFORE OF ALL! IMPORTANT
 $xWAF = new xWAF();
 $xWAF->start();
 
+// Done, Blocking XSS, SQL, and HTML Malicious code.
+
 ?>
 <title>xWAF Test</title>
 
@@ -14,7 +16,9 @@ $xWAF->start();
 	if (isset($_POST['csrf'])) {
 		// Aright! Form Requested.
 		if ($xWAF->verifyCSRF($_POST['csrf'])) {
-			echo "Form Validation Completed without Errors or Vulns!";
+			echo "Form Validation Completed without Errors or Vulns!<br><br><br>";
+			$Post = print_r($_POST);
+			echo "<code>". $Post ."</code>";
 		} else {
 			echo "Invalid CSRF Token!";
 		}
